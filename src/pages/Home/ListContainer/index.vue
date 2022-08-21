ac62c794f8cd2995eae5091c8cbab3e7a9160609
lklzc1012 <88605442+lklzc1012@users.noreply.github.com>
2022-08-21 17:38:44 +0800

    Add files via upload

---
+++ src/pages/Home/ListContainer/index.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="list-container">
+    <div class="sortList clearfix">
+      <div class="center">
+        <Carousel :list="bannerList"/>
+      </div>
+      <div class="right">
+        <div class="news">
+          <h4>
+            <em class="fl">商城快报</em>
+            <span class="fr tip">更多 ></span>
+          </h4>
+          <div class="clearix"></div>
+          <ul class="news-list unstyled">
+            <li>
+              <span class="bold">[公告]</span>备战开学季 全民半价购数码
+            </li>
+            <li>
+              <span class="bold">[公告]</span>备战开学季 全民半价购数码
+            </li>
+            <li>
+              <span class="bold">[公告]</span>备战开学季 全民半价购数码
+            </li>
+            <li>
+              <span class="bold">[公告]</span>备战开学季 全民半价购数码
+            </li>
+            <li>
+              <span class="bold">[公告]</span>备战开学季 全民半价购数码
+            </li>
+          </ul>
+        </div>
+        <ul class="lifeservices">
+          <li class=" life-item ">
+            <i class="list-item"></i>
+            <span class="service-intro">话费</span>
+          </li>
+          <li class=" life-item ">
+            <i class="list-item"></i>
+            <span class="service-intro">机票</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">电影票</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">游戏</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">彩票</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">加油站</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">酒店</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">火车票</span>
+          </li>
+          <li class="life-item ">
+            <i class="list-item"></i>
+            <span class="service-intro">众筹</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">理财</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">礼品卡</span>
+          </li>
+          <li class="life-item">
+            <i class="list-item"></i>
+            <span class="service-intro">白条</span>
+          </li>
+        </ul>
+        <div class="ads">
+          <img src="./images/ad1.png"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+
+export default {
+  name: "index",
+  mounted() {
+    // 派发action
+    this.$store.dispatch('getBannerList')
+  },
+  computed: {
+    ...mapState({
+      bannerList: state => state.Home.bannerList
+    })
+  }
+}
+</script>
+
+<style scoped lang="less">
+.list-container {
+  width: 1200px;
+  margin: 0 auto;
+
+  .sortList {
+    height: 464px;
+    padding-left: 210px;
+
+    .center {
+      box-sizing: border-box;
+      width: 740px;
+      height: 100%;
+      padding: 5px;
+      float: left;
+    }
+
+    .right {
+      float: left;
+      width: 250px;
+
+      .news {
+        border: 1px solid #e4e4e4;
+        margin-top: 5px;
+
+        h4 {
+          border-bottom: 1px solid #e4e4e4;
+          padding: 5px 10px;
+          margin: 5px 5px 0;
+          line-height: 22px;
+          overflow: hidden;
+          font-size: 14px;
+
+          .fl {
+            float: left;
+          }
+
+          .fr {
+            float: right;
+            font-size: 12px;
+            font-weight: 400;
+          }
+        }
+
+        .news-list {
+          padding: 5px 15px;
+          line-height: 26px;
+
+          .bold {
+            font-weight: 700;
+          }
+        }
+      }
+
+      .lifeservices {
+        border-right: 1px solid #e4e4e4;
+        overflow: hidden;
+        display: flex;
+        flex-wrap: wrap;
+
+        .life-item {
+          border-left: 1px solid #e4e4e4;
+          border-bottom: 1px solid #e4e4e4;
+          margin-right: -1px;
+          height: 64px;
+          text-align: center;
+          position: relative;
+          cursor: pointer;
+          width: 25%;
+
+          .list-item {
+            background-image: url(~@/assets/images/icon.png);
+            width: 50%;
+            height: 50%;
+            background-size: 100% 100%;
+            display: block;
+            margin: 5px auto 1px auto;
+          }
+
+          .service-intro {
+            line-height: 22px;
+            width: 60px;
+            display: block;
+          }
+        }
+      }
+
+      .ads {
+        margin-top: 5px;
+
+        img {
+          opacity: 0.8;
+          transition: all 400ms;
+
+          &:hover {
+            opacity: 1;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
