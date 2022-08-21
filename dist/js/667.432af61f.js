f6193b5e903b62eccc0c06edd612b9a88ecb9ef5
lklzc1012 <88605442+lklzc1012@users.noreply.github.com>
2022-08-21 18:45:24 +0800

    Delete dist directory

--- dist/js/667.432af61f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkapp"]=self["webpackChunkapp"]||[]).push([[667],{1667:function(n,e,u){u.r(e),u.d(e,{default:function(){return c}});var t=function(){var n=this,e=n._self._c;return e("div",[n._v(" 团购订单内容 ")])},r=[],a={name:"index"},l=a,p=u(1001),s=(0,p.Z)(l,t,r,!1,null,"50412b72",null),c=s.exports}}]);
-//# sourceMappingURL=667.432af61f.js.map
